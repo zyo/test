1e60373b7f98f0762595b4f982d5b9acccd35838
zyo <zyo@W2>
2021-11-03 09:49:32 +0200

    example

---
+++ app.js
@@ -0,0 +1,26 @@
+
+function _view_x3(camera_id)
+{
+  // -- bind viewpoint
+  const v = document.getElementById(camera_id);
+  v.setAttribute("centerOfRotation", "0 0 0");
+  v.setAttribute("position", "0 0 0");
+  v.setAttribute('set_bind','true');
+}   
+
+function draw_plane(x, y) {
+  // --
+  const x3 = document.getElementById('x3d_test');
+  // -- 
+  const xx = x3.querySelector("plane[def='base_plan_grid']");
+  if (!xx) return false;
+  xx.setAttribute("size", (x + 2) + " " + (y + 2));
+  xx.setAttribute("subdivision", (x + 2) + " " + (y + 2));
+}
+
+/** */
+window.onload = function () {
+  console.log('start ' + document.title);
+  _view_x3('camera_main');
+  draw_plane (8,4);
+}
